4a2bd01877b16fe09d41f89b24dc3ba1310c47b2
Marc Kevin <mbarrioga@hawk.iit.edu>
2022-04-13 23:20:21 -0500

    Update index.js
    added map constructor and options variable

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 function initMap(){
-    
+  var options = {
+    zoom:12,
+    center:{lat:41.8089, lng:-88.0112}
+  }
+
+  var map = new google.maps.Map(document.getElementById('map'), options);
 }
