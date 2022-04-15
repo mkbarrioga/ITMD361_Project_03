15a7f8a651227e449e2105b71f8328e6920524b0
Marc Kevin <mbarrioga@hawk.iit.edu>
2022-04-14 20:03:57 -0500

    Update index.js
    added auto complete direction function in maps

--- js/index.js
+++ js/index.js
@@ -16,14 +16,15 @@ function initMap(){
       marker.setIcon(attrib.iconImage);
     }
 
-    if(attrib.content){
+    if(attrib.content && attrib.message){
       var infoWindow = new google.maps.InfoWindow({
         content:attrib.content
       });
 
       marker.addListener('click', function(){
         infoWindow.open(map, marker);
-        document.getElementById('captions').innerHTML = attrib.content;
+        document.getElementById('markerTitle').innerHTML = attrib.content;
+        document.getElementById('captions').innerHTML = attrib.message;
       });
     }
 
@@ -31,7 +32,7 @@ function initMap(){
       marker.setAnimation(attrib.animation);
     }
   }
-  
+
   var markers = [
     {
       coords:{lat:41.8314798,lng:-87.6283148},
@@ -44,7 +45,7 @@ function initMap(){
       coords:{lat:41.9876626,lng:-87.6545129},
       iconImage:'images/beach.PNG',
       content:'<p>Kathy Osterman Beach</p>',
-      message: 'It is also called Hollywood Beach by locals. I spend times here to take my dog for a walk, read, or mostly just feel the sand on my feet'
+      message: 'It is also called Hollywood Beach by locals. I spend times here to take my dog for a walk, read, or mostly just feel the sand on my feet.'
     },
     {
       coords:{lat:41.8663134,lng:-87.6070143},
@@ -81,4 +82,111 @@ function initMap(){
   for(var i=0; i<markers.length; i++){
     addMarker(markers[i]);
   }
+
+  new AutocompleteDirectionsHandler(map);
 }
+
+class AutocompleteDirectionsHandler {
+  map;
+  originPlaceId;
+  destinationPlaceId;
+  travelMode;
+  directionsService;
+  directionsRenderer;
+  constructor(map) {
+    this.map = map;
+    this.originPlaceId = "";
+    this.destinationPlaceId = "";
+    this.travelMode = google.maps.TravelMode.WALKING;
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsRenderer = new google.maps.DirectionsRenderer();
+    this.directionsRenderer.setMap(map);
+
+    const originInput = document.getElementById("origin-input");
+    const destinationInput = document.getElementById("destination-input");
+    const modeSelector = document.getElementById("mode-selector");
+    // Specify just the place data fields that you need.
+    const originAutocomplete = new google.maps.places.Autocomplete(
+      originInput,
+      { fields: ["place_id"] }
+    );
+    // Specify just the place data fields that you need.
+    const destinationAutocomplete = new google.maps.places.Autocomplete(
+      destinationInput,
+      { fields: ["place_id"] }
+    );
+
+    this.setupClickListener(
+      "changemode-walking",
+      google.maps.TravelMode.WALKING
+    );
+    this.setupClickListener(
+      "changemode-transit",
+      google.maps.TravelMode.TRANSIT
+    );
+    this.setupClickListener(
+      "changemode-driving",
+      google.maps.TravelMode.DRIVING
+    );
+    this.setupPlaceChangedListener(originAutocomplete, "ORIG");
+    this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
+      destinationInput
+    );
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+  }
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  setupClickListener(id, mode) {
+    const radioButton = document.getElementById(id);
+
+    radioButton.addEventListener("click", () => {
+      this.travelMode = mode;
+      this.route();
+    });
+  }
+  setupPlaceChangedListener(autocomplete, mode) {
+    autocomplete.bindTo("bounds", this.map);
+    autocomplete.addListener("place_changed", () => {
+      const place = autocomplete.getPlace();
+
+      if (!place.place_id) {
+        window.alert("Please select an option from the dropdown list.");
+        return;
+      }
+
+      if (mode === "ORIG") {
+        this.originPlaceId = place.place_id;
+      } else {
+        this.destinationPlaceId = place.place_id;
+      }
+
+      this.route();
+    });
+  }
+  route() {
+    if (!this.originPlaceId || !this.destinationPlaceId) {
+      return;
+    }
+
+    const me = this;
+
+    this.directionsService.route(
+      {
+        origin: { placeId: this.originPlaceId },
+        destination: { placeId: this.destinationPlaceId },
+        travelMode: this.travelMode,
+      },
+      (response, status) => {
+        if (status === "OK") {
+          me.directionsRenderer.setDirections(response);
+        } else {
+          window.alert("Directions request failed due to " + status);
+        }
+      }
+    );
+  }
+}
+
+window.initMap = initMap;
